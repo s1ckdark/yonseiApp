2c91f2a1575a26d7492e8bdf83b78eef2b7a7713
dave <dave.lee@uniquedonut.com>
2019-06-15 15:14:36 +0900

    hm need to fix delete store, signup and profile edit

--- app/src/main/java/com/example/yonseiapp/activities/Stores/StoreManagerActivity.java
+++ app/src/main/java/com/example/yonseiapp/activities/Stores/StoreManagerActivity.java
@@ -1,5 +1,6 @@
 package com.example.yonseiapp.activities.Stores;
 
+import java.io.IOException;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,19 +8,10 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.yonseiapp.R;
 import com.example.yonseiapp.db.StoreTable;
-import com.example.yonseiapp.utils.Utils;
-
-import org.json.JSONObject;
-
-import java.io.IOException;
-
-import okhttp3.Call;
-import okhttp3.Response;
 
 public class StoreManagerActivity extends AppCompatActivity {
     StoreAdapter adapter = null;
@@ -29,44 +21,37 @@ public class StoreManagerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store_manager);
 
-        ((TextView) findViewById(R.id.topTitle)).setText("상점관리");
-//        findViewById(R.id.topRight).setVisibility(View.GONE);
+        ((TextView)findViewById(R.id.topTitle)).setText("상점관리");
+//      findViewById(R.id.topRight).setVisibility(View.GONE);
 
-        TextView topRight = ((TextView) findViewById(R.id.topRight));
+        TextView topRight = ((TextView)findViewById(R.id.topRight));
         topRight.setText("상점추가");
-
         topRight.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 Intent i = new Intent(StoreManagerActivity.this, StoreAddActivity.class);
                 startActivity(i);
             }
         });
 
+
         if (StoreTable.inst().size() == 0)
             putStores();
 
+        ((RecyclerView)findViewById(R.id.recyclerView)).setAdapter(new StoreAdapter());
+
+
         adapter = new StoreAdapter(new StoreAdapter.DelCallBack() {
             @Override
-            public void del(final int idx) {
-                new AlertDialog.Builder(StoreManagerActivity.this)
-                        .setMessage("지우시겠습니까?")
-                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                StoreTable.inst().del(idx);
-                                adapter.notifyDataSetChanged();
-                            }
-                        }).setNegativeButton("취소", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.dismiss();
-                    }
-                }).show();
+            public void del(int idx) {
+                   new AlertDialog().Builder(StoreManagerActivity.this)
+                          .setMessage("지우시겠습니까?")
+                StoreTable.inst().del(idx);
+                adapter.notifyDataSetChanged();
             }
         });
-
-        ((RecyclerView)findViewById(R.id.recyclerView)).setAdapter(new StoreAdapter());
+//
+        ((RecyclerView)findViewById(R.id.recyclerView)).setAdapter(adapter);
     }
 
     private void putStores() {
